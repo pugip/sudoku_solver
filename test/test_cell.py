3e843a725bd5549038c483dbaed578c596c69e6e
Dan Clark <wpu.clark@gmail.com>
2019-07-08 20:49:36 -0400

    added more tests

--- test/test_cell.py
+++ test/test_cell.py
@@ -1,19 +1,22 @@
 from lib.cell import Cell
 class TestCell:
 
+  def test_use_none_value(self):
+    b = Cell(None, 1, 1)
+    assert b.value == None 
+
+  def test_get_value(self):
+    b = Cell(4, 1, 1)
+    assert b.value == 4
   
-# methods
-# get_row
-# get_col
-# get_block
   def test_get_row(self):
     b = Cell(4, 1, 1)
-    assert b.row == 1
+    assert b.x_coord == 1
 
   def test_get_col(self):
     b = Cell(4, 1, 1)
-    assert b.col == 1
+    assert b.y_coord == 1
 
   def test_get_block(self):
-    b = Cell(4, 1, 1)
-    assert b.block == 1
+    b = Cell(4, 4, 8)
+    assert b.block() == 5
--- test/test_puzzle.py
+++ test/test_puzzle.py
@@ -1,4 +1,24 @@
+from lib.puzzle import Puzzle
 class TestPuzzle:
 
   def test_first(self):
     assert 5 != 4 
+
+  def test_valid(self):
+    p=('... ..3 19.'
+       '2.. .1. .6.'
+       '..1 92. 5..'
+  
+       '5.2 6.7 ..1'
+       '673 .9. ..5'
+       '.98 ..4 67.'
+  
+       '32. 14. ..6'
+       '.56 ... 21.'
+       '... 5.. ...')
+    p = p.replace(' ', '')
+    print(p)
+    puzzle = Puzzle(p)
+    print(puzzle.cells)
+    assert puzzle.valid() 
+
