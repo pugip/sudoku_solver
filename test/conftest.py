2a49824bb7e937f56f2de518eec6b09ca788e352
Dan Clark <wpu.clark@gmail.com>
2019-07-09 21:16:18 -0400

    Add some invalid puzzle fixtures and tests for them.

--- test/conftest.py
+++ test/conftest.py
@@ -17,3 +17,51 @@ def valid_puzzle():
           '... 5.. ...')
     ps = ps.replace(' ', '')
     yield Puzzle(ps)
+
+@pytest.fixture
+def invalid_column_puzzle():
+    ps = ('5.. ..3 19.'
+          '2.. .1. .6.'
+          '..1 92. 5..'
+
+          '5.2 6.7 ..1'
+          '673 .9. ..5'
+          '.98 ..4 67.'
+
+          '32. 14. ..6'
+          '.56 ... 21.'
+          '... 59. .7.')
+    ps = ps.replace(' ', '')
+    yield Puzzle(ps)
+
+@pytest.fixture
+def invalid_block_puzzle():
+    ps = ('... ..3 19.'
+          '2.. .1. .6.'
+          '..1 92. 5..'
+
+          '5.2 6.7 ..1'
+          '673 49. ..5'
+          '.98 ..4 67.'
+
+          '32. 14. ..6'
+          '.56 ... 21.'
+          '... 5.. .2.')
+    ps = ps.replace(' ', '')
+    yield Puzzle(ps)
+
+@pytest.fixture
+def invalid_row_puzzle():
+    ps = ('.9. ..3 19.'
+          '2.. .1. .6.'
+          '..1 92. 5..'
+
+          '5.2 6.7 ..1'
+          '673 .9. 3.5'
+          '.98 ..4 67.'
+
+          '32. 14. ..6'
+          '.56 ... 21.'
+          '... 5.. 5..')
+    ps = ps.replace(' ', '')
+    yield Puzzle(ps)
--- test/test_puzzle.py
+++ test/test_puzzle.py
@@ -1,10 +1,21 @@
 from lib.puzzle import Puzzle
 class TestPuzzle:
 
-  def test_first(self):
-    assert 5 != 4 
+    def test_first(self):
+        assert 5 != 4
 
-  def test_valid(self, valid_puzzle):
-    print(valid_puzzle.cells)
-    assert valid_puzzle.valid()
+    def test_valid(self, valid_puzzle):
+        print(valid_puzzle.cells)
+        assert valid_puzzle.valid()
 
+    def test_valid_with_invalid_columns(self, invalid_column_puzzle):
+        print(invalid_column_puzzle.cells)
+        assert not invalid_column_puzzle.valid()
+
+    def test_valid_with_invalid_rows(self, invalid_row_puzzle):
+        print(invalid_row_puzzle.cells)
+        assert not invalid_row_puzzle.valid()
+
+    def test_valid_with_invalid_blocks(self, invalid_block_puzzle):
+        print(invalid_block_puzzle.cells)
+        assert not invalid_block_puzzle.valid()
